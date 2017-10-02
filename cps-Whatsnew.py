fead33d844164325a5c55e19437f3978a162787f
bodybybuddha <jtaylor@recycledpapyr.us>
2017-10-02 16:00:54 -0400

    Added marrow.mailer & Jinja2 templating to render and send newsletters.

--- cps-Whatsnew.py
+++ cps-Whatsnew.py
@@ -2,23 +2,22 @@ import feedparser
 import os
 import json
 import logging.config
+
 from feedparser import _parse_date as parse_date
 from time import mktime
 from datetime import datetime, timedelta
+from marrow.mailer import Mailer, Message
+import jinja2
 
 gConfig = None
 gLogger = None
 gNewBookTable = None
 
-# modify this to change the Template Directory
-TEMPLATE_DIR = '/templates/'
-COVER_DIR = '/covers/'
-
 
 def setup_logging(
-    default_path='logging.json',
-    default_level=logging.INFO,
-    env_key='LOG_CFG'
+        default_path='logging.json',
+        default_level=logging.INFO,
+        env_key='LOG_CFG'
 ):
     """Setup logging configuration
 
@@ -33,7 +32,7 @@ def setup_logging(
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=default_level,
-                            filename=__file__+'.log',
+                            filename=__file__ + '.log',
                             format='%(asctime)s : %(levelname)s : %(name)s  : %(message)s')
 
 
@@ -51,20 +50,26 @@ def getnewbooks(
                          + gConfig['serveraddress'])
 
     gLogger.info('Name of the feed:' + d.feed.title)
+    gLogger.info('Looking for books uploaded in the last: ' + str(gConfig['numofdaysfornotification']) + ' days.')
 
-    for book in d.entries:
-        dt = datetime.fromtimestamp(mktime(parse_date(book.updated)))
-        if datetime.now() - dt < timedelta(days=int(gConfig['numofdaysfornotification'])):
-            if 'title' in book:
-                print 'Title: ' + book.title
-            if 'authors' in book:
-                print 'Author(s): ' + book.author
-            if 'summary' in book:
-                print 'Summary: ' + book.summary
-            if 'links' in book:
-                print 'link to icon: ' + book.links[1].href.replace("/cover/", "/thumb_240_240/")
-            else:
-                print 'No Summary'
+    recentbooks = []
+
+    if d.status == 200:
+        for book in d.entries:
+            dt = datetime.fromtimestamp(mktime(parse_date(book.updated)))
+            if datetime.now() - dt < timedelta(days=int(gConfig['numofdaysfornotification'])):
+                if 'title' in book:
+                    gLogger.info('Found book. Title: ' + book.title)
+                else:
+                    gLogger.info('Found book. Strange, no Title field!')
+
+                # add newly added book to array for use later
+                recentbooks.append(book)
+
+        return recentbooks
+
+    else:
+        gLogger.error('Error getting opds feed! - Please check config. Status Code: ' + str(d.status))
 
 
 def getconfig(
@@ -80,8 +85,63 @@ def getconfig(
         with open('config.json', 'r') as f:
             gConfig = json.load(f)
             gLogger.info('Opened config file: config.json.')
-    except :
+    except:
         gLogger.exception('Error getting config file: config.json opened.')
+
+
+def buildnewsletter(
+            book_list
+
+):
+    """Routine to send an HTML newsletter
+
+
+    """
+    gLogger.info('Pulling together the newsletter.')
+
+    mailer = Mailer(dict(
+            transport=dict(
+                use='smtp',
+                host=gConfig['SMTPSettings']['host'],
+                port=gConfig['SMTPSettings']['port'],
+                username=gConfig['SMTPSettings']['user'],
+                password=gConfig['SMTPSettings']['password'],
+                tls=gConfig['SMTPSettings']['startttls']
+            )
+        )
+    )
+
+    try:
+        cd = os.path.dirname(os.path.abspath(__file__))
+
+        # Perform jinja
+
+        jinjaenv = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(cd)
+        )
+
+        messagebody = jinjaenv.get_template(gConfig['TEMPLATE_FILE']).render(
+            book_list=book_list
+        )
+
+        mailer.start()
+
+        for winner in gConfig['DistributionList']:
+
+            message = Message(author=gConfig['SMTPSettings']['user'], to=winner['addr'])
+            message.subject = gConfig['SMTPSettings']['subject']
+            message.plain = "This is only exciting if you use an HTML capable email client. Please disregard."
+            message.rich = messagebody
+
+            mailer.send(message)
+    except:
+        gLogger.exception('Error sending email.')
+
+    mailer.stop()
+
+    gLogger.info('Completed newsletter routine.')
+
+    return
 
 
 def main():
@@ -97,12 +157,16 @@ def main():
     getconfig()
 
     if gConfig:
-        getnewbooks()
+        gNewBookTable = getnewbooks()
+
+        if gNewBookTable:
+            gLogger.info("We found " + str(len(gNewBookTable)) + ' new books.')
+            buildnewsletter(gNewBookTable)
+        else:
+            gLogger.info("We didn't find any books.")
 
     gLogger.info('Finishing script.')
 
+
 if __name__ == "__main__":
     main()
-
-
-
