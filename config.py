cca98dece22ae1555372d75d6fdf2a6edc1143ab
bodybybuddha <jtaylor@recycledpapyr.us>
2017-11-24 16:27:28 -0500

    Logic added for types of DL sources

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 
 import logging
 import json
+import os
+
 
 logger = logging.getLogger(__name__)
 settings = None
@@ -19,5 +21,21 @@ def get_config(
         with open('config.json', 'r') as f:
             settings = json.load(f)
             logger.info('Opened config file: config.json.')
+
+        # Since we got the configuration - let's check for the database config setting
+        # and do some db operations if needed
+
+        if settings['DLSource'] == 'db':
+
+            logger.info('Configured for database')
+
+            if os.path.exists(settings['Database']['extras_db_fn']):
+                logger.debug("Found database file at {0}".format(settings['Database']['extras_db_fn']))
+
+            else:
+                logger.error("Did NOT Find database file at {0}".format(settings['Database']['extras_db_fn']))
+
+                logger.exception("")
+
     except:
         logger.exception('Error getting config file: config.json opened.')
